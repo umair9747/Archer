62f2b3a2685be86cf650d710f516c4f845e5b035
Qexat <contact@qexat.com>
2022-01-06 15:36:21 +0100

    syntax enhancement + used chalk

--- initials/banner.js
+++ initials/banner.js
@@ -1,26 +1,39 @@
 //Import starts
-var figlet = require('figlet');
-var colors = require('colors');
-var align = require('align-text');
+const figlet = require("figlet");
+const align = require("align-text");
+const chalk = require("chalk");
 //Import ends
 
-function data(){
-figlet('-ARCHER->', function(err, data) {
+async function data() {
+  figlet("-ARCHER->", (err, data) => {
     if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
+      console.log("Something went wrong...");
+      console.dir(err);
+      return;
     }
-    console.log(align(data.green, 12))
-    console.log(align("A Tool to Check for Response Status Codes".green, 15))
-    console.log(align("Developed by Umair Nehri (0x9747)".green, 32))
+    console.log(align(chalk.green(data), 12));
+    console.log(align("A Tool to Check for Response Status Codes", 15));
+    console.log(align(chalk.green("Developed by Umair Nehri (0x9747)\n"), 32));
+    console.log(
+      align(
+        chalk.blue(
+          '"It is sometimes an appropriate response to reality to go insane." ~Philip K.'
+        ),
+        4
+      )
+    );
     console.log("");
-    console.log(align("\"It is sometimes an appropriate response to reality to go insane.\" ~Philip K.".blue, 4));
-    console.log("");
-    console.log(align("[Use --help or -h flag for getting information about command usage]".green, 6));
+    console.log(
+      align(
+        chalk.green(
+          "[Use --help or -h flag for getting information about command usage]"
+        ),
+        6
+      )
+    );
 
     console.log("");
-    });
+  });
 }
 
-module.exports = {data};
+module.exports = { data };
