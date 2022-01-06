fea7ed1949ea114672da918208f707007a5959c0
Qexat <contact@qexat.com>
2022-01-06 15:31:19 +0100

    function timeout does not need to last 3 seconds

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const banner = require('./initials/banner');
 const input = require('./functions/process');
 
-banner.data();
-setTimeout(input.process, 3000);
+banner.data()
+
+setTimeout(input.process, 10);
